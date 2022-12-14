483fbe78c32ff02bda0ff6d8b648974bb09aee61
Adrian Piedra <adrian.piedra@wizeline.com>
2022-12-14 17:29:13 -0600

    Added progress view.

--- app/src/main/java/com/example/triviakotlincourse/components/QuestionsView.kt
+++ app/src/main/java/com/example/triviakotlincourse/components/QuestionsView.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +20,7 @@ import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,9 +49,7 @@ fun QuestionsView(viewModel: QuestionsViewModel) {
 
         if (questions != null) {
             QuestionDisplay(
-                question = question!!,
-                questionIndex = questionIndex,
-                viewModel = viewModel
+                question = question!!, questionIndex = questionIndex, viewModel = viewModel
             ) {
                 questionIndex.value++
             }
@@ -102,7 +99,11 @@ fun QuestionDisplay(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
-            QuestionTracker(counter = questionIndex.value, outOff = viewModel.data.value.data?.count() ?: 0)
+            if (questionIndex.value >= 3) ShowProgress(score = questionIndex.value)
+
+            QuestionTracker(
+                counter = questionIndex.value, outOff = viewModel.data.value.data?.count() ?: 0
+            )
             DrawDottedLine(pathEffect = pathEffect)
             Column {
                 Text(
@@ -240,4 +241,65 @@ fun QuestionTracker(counter: Int, outOff: Int) {
             }
         }
     }, modifier = Modifier.padding(20.dp))
+}
+
+@Preview
+@Composable
+fun ShowProgress(score: Int = 12) {
+    val progressFactor by remember(score) {
+        mutableStateOf(score * 0.005f)
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .height(45.dp)
+            .border(
+                width = 4.dp, brush = Brush.linearGradient(
+                    colors = listOf(
+                        AppColors.mLightPurple, AppColors.mLightPurple
+                    )
+                ), shape = RoundedCornerShape(34.dp)
+            )
+            .clip(
+                RoundedCornerShape(
+                    topStartPercent = 50,
+                    topEndPercent = 50,
+                    bottomStartPercent = 50,
+                    bottomEndPercent = 50
+                )
+            )
+            .background(Color.Transparent), verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            contentPadding = PaddingValues(1.dp),
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth(progressFactor)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(0xFFF95075), Color(0xFFBE6BE5)
+                        )
+                    )
+                ),
+            enabled = false,
+            elevation = null,
+            colors = buttonColors(
+                backgroundColor = Color.Transparent, disabledBackgroundColor = Color.Transparent
+            )
+        ) {
+            Text(
+                text = (score * 10).toString(),
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(23.dp))
+                    .fillMaxHeight(0.87f)
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                color = AppColors.mOffWhite,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
